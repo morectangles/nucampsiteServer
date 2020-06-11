ca20256e14e12a2ce9a77ead4710d5e1127396f9
Katie Konrad <kathrynkonrad@gmail.com>
2020-06-10 20:39:01 -0400

    Passport JWT

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-//cookies 
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
 //passport authentication
 const passport = require('passport');
-const authenticate = require('./authenticate');
+const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,7 +14,7 @@ const partnerRouter = require('./routes/partnerRouter');
 
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/nucampsite';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useFindAndModify: false,
@@ -42,38 +38,14 @@ app.use(express.urlencoded({ extended: false }));
 //cookies used - key as parameter
 //app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
 
 //passport - only necessary if you are using session-based authentication, 
 //2 middleware functions provided by passport to check incoming requests to see
 //if there's an existing session for the client, if so, it comes in as req.user
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-//authentication
-function auth(req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    const err = new Error('You are not authenticated!');
-    err.status = 401;
-    return next(err);
-  } else {
-    return next();
-  } 
-}
-
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- authenticate.js
+++ authenticate.js
@@ -1,7 +1,49 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
+const JwtStrategy = require('passport-jwt').Strategy; //constructor
+const ExtractJwt = require('passport-jwt').ExtractJwt; //object that will provide us with several helper methods
+//one of which we'll use later to extract jw token from a req obj
+const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+
+const config = require('./config.js');
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+
+exports.getToken = function(user) {
+    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
+};
+
+//configure jwt strategy for passport - opts is options
+const opts = {};
+//using one of the helper methods from extractjwt - specifies how JWT should be extracted - simplest method
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+//lets us supply jwt strategy with a key with which we'll assign the token
+opts.secretOrKey = config.secretKey;
+
+//exporting jwt strategy
+exports.jwtPassport = passport.use(
+    new JwtStrategy(
+        opts,
+        (jwt_payload, done) => {
+            console.log('JWT payload:', jwt_payload);
+            User.findOne({_id: jwt_payload._id}, (err, user) => {
+                if (err) {
+                    //(error?, user?)
+                    return done(err, false);
+                } else if (user) {
+                    return done(null, user);
+                } else {
+                    return done(null, false);
+                }
+            });
+        }
+    )
+);
+
+//verify that an incoming request is from a user - using jwt strategy, not sessions
+//setting it up here to use in other modules whenever we want to authenticate w/ JWT
+exports.verifyUser = passport.authenticate('jwt', {session: false});
