eac09473f01eb23501c41b25b11610e9872f1150
Katie Konrad <kathrynkonrad@gmail.com>
2020-06-19 18:22:39 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '299603861075361',
+        clientSecret: 'c7522e7103bb3df52a932ff12003cf38'
+    }
 }
